7ea4ee211c175cdf2be02c91e3ea72fb553e0490
tangbeve <tlbeverly96@gmail.com>
2019-11-20 08:32:12 -0500

    first commit

---
+++ my-angular-albums/src/app/albums/album.model.ts
@@ -0,0 +1,16 @@
+
+export interface Album {
+  id: number;
+  artist: string;
+  albumName: string;
+  genre: string;
+  price: number;
+  currency?: string;
+  onSale: boolean;
+  year: number;
+  releaseDate: string;
+  recordingLocation: string;
+  duration: string;
+  url: string;
+}
+
